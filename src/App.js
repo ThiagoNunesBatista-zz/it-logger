1c9b1b1e27a25f624c383986ccfec2ab4ecd6fc4
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-05-22 17:33:13 -0300

    Materialize CSS Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 
 function App () {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit()
+  }, [])
+
   return (
     <>
       <h1>Hello World</h1>
